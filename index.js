aae7d3f60f3cc37a64bbf0bedefe34832c24ffea
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-14 17:38:58 -0400

    commit tree 339b5fff6bc3da821c39a6146b0367341416512c

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ document.querySelector("button").addEventListener("click", handleClick);
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
-}
+ }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timestr) {
+  const hour = parseInt(timestr);
+  if ( hour < 12 ) return 'Good Morning'
+  
+  if (hour > 17) return 'Good Evening'
+  return 'Good Afternoon'
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById('greeting').innerText = msg;
+}
